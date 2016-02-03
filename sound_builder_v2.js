4c022505daeb50fc981d84918b5fb07df8e4e8a4
alxnery <a.nery123@gmail.com>
2016-02-03 12:47:23 -0500

    fixed sound_builder

--- sound_builder_v2.js
+++ sound_builder_v2.js
@@ -1,34 +1,34 @@
 	var bsl = function(callback){
 
-	this.fs = require('fs');
-
-	this.sounds = {
-		random:{}
-	};
-
-	this.sounds_list = {};
-
+	var fs = fs || require('fs');
 	//loadlist
 	this.loadList = function (){
 
 	try{
 	this.sounds_list = require('./sounds_list.json');
+	//console.log("sounds list imported");
 	} catch(e){
-	this.fs.writeFile("./sounds_list.json", 
+	fs.writeFile("./sounds_list.json", 
 		JSON.stringify(this.sounds,null,4), 
 		function(err){
-			console.log("sounds_list.json populated");
+			console.log("sounds_list.json populated, rerun program");
 			this.sounds_list = require('./sounds_list.json');
+			process.exit();
 		});
 	}
-
-	}
-
 	//variables
 	this.mp3 = /.*mp3/i;
 	this.base_dir = "./sounds/";
 	this.sounds = this.sounds_list;
+
+	if(!this.isKeyPresent(this.sounds, "random")){
 	this.random = this.sounds.random = {};
+	}
+	else{
+	this.random = this.sounds.random;
+	}
+
+	}
 
 	/*
 		This wrapper function is only implemented to sort any loose files in the root path to 
@@ -36,41 +36,48 @@
 	*/
 	this.startScan = function (path, catchAll, curObject, curObject_list){
 		
-		var files = this.fs.readdirSync(path);
-		var i=0;
+		var fs = fs || require('fs');
+		var files = fs.readdirSync(path);
 		for(i=0; i<files.length; i++){
 			
-			var stats = this.fs.statSync(path+files[i]);
+			var stats = fs.statSync(path+files[i]);
 
 			if(stats.isFile()){
 				if(this.mp3.test(files[i])){
 					var pretty_file = files[i].replace(".mp3","");
 					
 					if(!(this.isKeyPresent(catchAll, pretty_file)) ) {
+						console.log("new file "+pretty_file+" added to "+this.base_dir);
 						this.appendKeytoObject(catchAll, pretty_file, this.base_dir);
 					}	
 				}
 			}
 			else if(stats.isDirectory()){
+
 				var pretty_path = files[i].replace("/","");
-				curObject[pretty_path] = {};
-				
-				var curobj = curObject[pretty_path];
 
-				if(this.isKeyPresent(curObject_list, pretty_path ) ){
-					console.log(pretty_path + " found");
-					console.log("current directory structure")
-					var curobj_list = curObject_list[pretty_path];
+					if(this.isKeyPresent(curObject_list, pretty_path ) ){
+						//console.log(pretty_path + " found");
+						//console.log("current directory structure")
+						var curobj_list = curObject_list[pretty_path];
+						var curobj = curObject[pretty_path];
 
-					this.readandADD(path + files[i]+ '/', curobj, curobj_list, true);
-				
-				}
+						this.readandADD(path + files[i]+ '/', curobj, curobj_list, true);
+						
+					}
 				else{
+
+					curObject[pretty_path] = {};
+					var curobj = curObject_list[pretty_path];
+					console.log("new path found "+pretty_path+" added to "+ this.base_dir);
+
 					this.readandADD(path + files[i]+'/', curobj, curObject_list, false);
 				}
 			}
+		}//end for
+
+		return curObject;
 		}
-	}
 
 	/*
 		@param
@@ -87,12 +94,15 @@
 				Think all of the different /bin/ directories in your usr/local/
 	*/
 	this.readandADD = function (path, curObject, curObject_list, withFlag){
-		var files = this.fs.readdirSync(path);
+
+		var fs = fs || require('fs');
+		console.log("beginning read and add on " + path);
+		var files = fs.readdirSync(path);
 		var i=0;
 
 		for(i=0; i<files.length; i++){
 
-			var stats = this.fs.statSync(path + files[i]);
+			var stats = fs.statSync(path + files[i]);
 			//if the file is a file
 			if(stats.isFile()){
 				//if the file is an mp3
@@ -102,11 +112,13 @@
 
 					//if we are in a brand new branch of the tree
 					if(!withFlag){
+						console.log("new file "+pretty_file+" added to "+path+files[i]);
 						this.appendKeytoObject(curObject, pretty_file, path);	
 					}
 					//if we are still in a folder that corresponds with the old json data, AND
 					//the key is unique, go ahead and add it
 					else if( (withFlag) && (!(this.isKeyPresent(curObject_list, pretty_file))) ){
+						console.log("new file "+pretty_file+" added to "+path+files[i]);
 						this.appendKeytoObject(curObject, pretty_file, path);
 					}
 					//if neither of these are true we can safely ignore the file
@@ -124,6 +136,8 @@
 					if(this.isKeyPresent(curObject_list, pretty_path))
 						this.readandADD(path + files[i]+ '/', curobj, curObject_list, true);
 				} else {
+						console.log("new path found "+pretty_path+" added to " + path+files[i]);
+
 						this.readandADD(path + files[i]+'/', curobj, curObject_list, false);
 				}
 			}//end isDirectory
@@ -152,12 +166,14 @@
 		};
 
 	}
+
 	this.buildList = function(callback){
+			var fs = fs || require('fs');
 			this.loadList();
 			this.startScan(this.base_dir, this.random, this.sounds, this.sounds_list);
-			console.log(JSON.stringify(this.sounds, null, 2));
+		//	console.log(JSON.stringify(this.sounds, null, 2));
 
-			this.fs.writeFile("./sounds_list.json", 
+			fs.writeFile("./sounds_list.json", 
 			JSON.stringify(this.sounds,null,4), 
 			function(err){
 				console.log("sounds_list.json populated");
